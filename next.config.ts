c776b06ed81896c435b6a243d54b1bf403271f25
gyuhwa9922 <rbghk1231@naver.com>
2026-06-14 06:18:48 +0900

    chore: 모바일 환경에서 테스트 환경을 위한 allowedDevorigins 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  // ⭐️ 장소가 바뀌어 IP가 변해도 모든 사설 IP 대역을 허용하는 와일드카드 방식
   allowedDevOrigins: ["172.30.*.*", "192.168.*.*", "10.*.*.*"],
 };
 
